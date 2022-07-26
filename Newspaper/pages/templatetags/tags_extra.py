4ee151ee9ec102f69ee2a7a99cbd3c5e7bd3ff70
m0nochr0mex <m0nochr0mex@gmail.com>
2022-07-26 23:53:12 +0800

    D6.6

--- Newspaper/pages/templatetags/tags_extra.py
+++ Newspaper/pages/templatetags/tags_extra.py
@@ -7,6 +7,11 @@ BADWORDS = ['carrot', 'radish']
 
 @register.filter(name='profanity')
 def profanity(value):
+    """
+    Very basic profanity filter
+    """
+
+    # TODO use regexp
     result = value
     for word in BADWORDS:
         result = result.replace(word, '#' * len(word))
--- readme.md
+++ readme.md
@@ -1,4 +1,9 @@
 # News portal (SF 07.2022)
 
 #### D5.9
+
 Commands for Django shell in cmd.py
+
+#### D6.6
+
+Censor filter is called `profanity`. Currently replacing words *radish* and *carrot*
