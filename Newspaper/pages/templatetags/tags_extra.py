86264f2aacb36ffeabf87f414605b2bb91fa15bf
m0no <m0nochr0mex@gmail.com>
2022-08-03 08:46:19 +0800

    fix urls in emails

--- Newspaper/pages/templatetags/tags_extra.py
+++ Newspaper/pages/templatetags/tags_extra.py
@@ -1,5 +1,6 @@
 import re
 from django import template
+from django.contrib.sites.models import Site
 
 register = template.Library()
 
@@ -16,3 +17,9 @@ def profanity(value):
     for word in BADWORDS:
         result = re.sub(word, '#' * len(word), result, flags=re.IGNORECASE)
     return result
+
+
+@register.filter
+def site_domain(path):
+    current_site = Site.objects.get_current()
+    return current_site.domain + path
