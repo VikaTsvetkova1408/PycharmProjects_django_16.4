2ee9521371a6073d3f2064b009f0d03f7d1f5269
m0no <m0nochr0mex@gmail.com>
2022-08-04 14:46:13 +0800

    fix config

--- Newspaper/Newspaper/settings.py
+++ Newspaper/Newspaper/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -205,6 +204,6 @@ CACHES = {
     'default': {
         'TIMEOUT': 60,
         'BACKEND': 'django.core.cache.backends.redis.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379',
+        'LOCATION': 'redis://localhost:6379',
     }
 }
