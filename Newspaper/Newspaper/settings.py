556323c774573e8975b93cd4da5693006c882afb
m0nochr0mex <m0nochr0mex@gmail.com>
2022-08-24 17:54:38 +0800

    logging configuration

--- Newspaper/Newspaper/settings.py
+++ Newspaper/Newspaper/settings.py
@@ -214,30 +214,102 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'formatters': {
-        'console': {
-            'format': '%(name)-12s %(levelname)-8s %(message)s'
+        'fmt_debug': {
+            'format': '%(levelname)s %(asctime)s %(message)s'
+        },
+        'fmt_info': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(message)s'
+        },
+        'fmt_warning': {
+            'format': '%(levelname)s %(asctime)s %(pathname)s %(message)s'
+        },
+        'fmt_err': {
+            'format': '%(levelname)s %(asctime)s %(pathname)s %(exc_info)d %(message)s'
         }
     },
     'filters': {
         'require_debug_true': {
             '()': 'django.utils.log.RequireDebugTrue'
+        },
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
         }
     },
     'handlers': {
-        'console': {
-            'level': 'INFO',
-            'filters': 'require_debug_true',
+        'console_dbg': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
             'class': 'logging.StreamHandler',
-            'formatter': 'console'
+            'formatter': 'fmt_debug'
+        },
+        'console_wrn': {
+            'level': 'WARNING',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'fmt_warning'
+        },
+        'console_err': {
+            'level': 'ERROR',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'fmt_err'
+        },
+        'file_general': {
+            'level': 'INFO',
+            'filters': ['require_debug_false'],
+            'class': 'logging.FileHandler',
+            'filename': '/tmp/logs/general.log',
+            'formatter': 'fmt_info'
+        },
+        'file_error': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'logging.FileHandler',
+            'filename': '/tmp/logs/errors.log',
+            'formatter': 'fmt_err'
+        },
+        'file_security': {
+            'level': 'INFO',
+            'filters': ['require_debug_false'],
+            'class': 'logging.FileHandler',
+            'filename': '/tmp/logs/security.log',
+            'formatter': 'fmt_info'
         },
         'mail_admins': {
-            'level': 'ERROR'
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
         }
     },
     'loggers': {
         'django': {
-            'handlers': ['console'],
+            'handlers': ['console_dbg', 'file_general', 'console_wrn', 'console_err'],
             'propagate': True
-        }
+        },
+        'django.request': {
+            'handlers': ['file_error', 'mail_admins'],
+            'propagate': True
+
+        },
+        'django.server': {
+            'handlers': ['file_error', 'mail_admins'],
+            'propagate': True
+
+        },
+        'django.template': {
+            'handlers': ['file_error'],
+            'propagate': True
+
+        },
+        'django.db_backends': {
+            'handlers': ['file_error'],
+            'propagate': True
+
+        },
+        'django.security': {
+            'handlers': ['file_security', 'mail_admins'],
+            'propagate': True
+
+        },
     }
 }
